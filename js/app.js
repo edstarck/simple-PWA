f833659d920d30ccf0c90d0a7fff8c4fe99ca6bb
edstarck <webeduard@yandex.ru>
2020-11-26 18:04:04 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+function createNode(element) {
+  return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+const ul = document.getElementById('people');
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+  })
+  .then((users) => {
+    return users.map((person) => {
+      let li = createNode('li'),
+        span = createNode('span');
+
+      li.innerHTML = person.name;
+      span.innerHTML = person.email;
+
+      append(li, span);
+      append(ul, li);
+    });
+  });
